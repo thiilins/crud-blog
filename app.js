f1f666b3852fd7cbe9b4f40e00dc5b2f00628241
Thiago Lins <taglins@gmail.com>
2021-10-14 20:55:22 -0300

    Estrutura básica do BackEnd

---
+++ app.js
@@ -0,0 +1,45 @@
+//  Importando e inicializando o módulo 'express'
+const express = require("express");
+const app = express();
+const port = 3000;
+const maintenance = false;
+//Importando Módulos
+const methodOverride = require("method-override");
+const indexRoutes = require("./src/routes");
+const path = require("path");
+const session = require("express-session");
+
+// Definindo View Engine
+app.set("view engine", "ejs");
+app.set("views", path.resolve("src", "views"));
+
+// Express Session
+app.use(
+  session({
+    secret: "DevHouseL3000TL",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+// Middlewares
+app.use(express.static(path.resolve("src", "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method")); //*?_method=PUT
+
+//Instanciando Rotas
+app.use("/", indexRoutes);
+
+// Manutenção
+app.use((req, res, next) => {
+  !maintenance ? next() : res.status(503).render("pages/maintenance");
+});
+// erro 404
+app.use((req, res, next) => {
+  res.status(404).render("pages/404");
+  next();
+});
+//Definindo escuta de porta para iniciar o servidor
+app.listen(port, () => {
+  console.log(`O Servidor está rodando em http://localhost:${port}`);
+});
---
+++ src/routes/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const IndexController = require("../controller/IndexController");
+
+router.get("/", IndexController.index);
+
+module.exports = router;
