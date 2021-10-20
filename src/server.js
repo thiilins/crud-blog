224c5eda61a1eb49404c943078cd3833901573e7
Thiago Lins <taglins@gmail.com>
2021-10-20 00:13:40 -0300

    Correções e Add de dependências

--- src/server.js
+++ src/server.js
@@ -1,9 +1,8 @@
-//  Importando e inicializando o módulo 'express'
 const express = require("express");
 const app = express();
 const port = 3000;
 const maintenance = false;
-//Importando Módulos
+
 const methodOverride = require("method-override");
 const indexRoutes = require("./src/routes");
 const adminRoutes = require("./src/routes/admin");
@@ -12,34 +11,35 @@ const path = require("path");
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-// Definindo View Engine
+const cors = require("cors");
 app.set("view engine", "ejs");
 app.set("views", path.resolve("src", "views"));
-app.use(cookieParser());
+
 app.use(logger("dev"));
-// Express Session
+app.use(cors);
 app.use(
   session({
-    secret: "DevHouseL3000TL",
+    secret: "CRUDBLOG-TD",
     resave: true,
     saveUninitialized: true,
+    cookie: {
+      secure: "auto",
+    },
   })
 );
-// Middlewares
 app.use(express.static(path.resolve("src", "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride("_method")); //*?_method=PUT
 
-//Instanciando Rotas
 app.use("/", indexRoutes);
 app.use("/", userRoutes);
 app.use("/admin", adminRoutes);
 
-// Manutenção
-app.use((req, res, next) => {
-  !maintenance ? next() : res.status(503).render("pages/maintenance");
-});
+// // Manutenção
+// app.use((req, res, next) => {
+//   !maintenance ? next() : res.status(503).render("pages/maintenance");
+// });
 // erro 404
 app.use((req, res, next) => {
   res.status(404).render("pages/404", { page: "404 - Página não encontrada" });
